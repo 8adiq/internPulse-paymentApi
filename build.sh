8da58113b3d7a6da9c5168248ae9a5ed7aae83e1
unknown <abdul.latif708@yahoo.com>
2025-08-24 19:06:02 +0000

    fixed db not openning

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ pip install -r requirements.txt
 # Create database directory if it doesn't exist
 mkdir -p db
 
+# Run migrations to create database
+python manage.py migrate --run-syncdb
+
 python manage.py collectstatic --no-input
-python manage.py migrate
